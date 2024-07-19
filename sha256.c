9cc87fedacbae7f7974045270052d9e1039aee1a
adam-younes <samakmashwee@gmail.com>
2024-07-18 22:46:22 -0500

    Header file specifications

--- sha256.c
+++ sha256.c
@@ -2,3 +2,16 @@
 #include <string.h>
 #include <stdint.h>
 
+#define DWORD 32
+#define RIGHTROTATE(word, bits) (((word) >> (bits)) | ((word) << (DWORD - (bits))))
+#define CH(x, y, z) (((x) & (y)) ^ (~(x) & (z)))
+#define MAJ(x, y. z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
+#define E0(x) (RIGHTROTATE(x, 2)) ^ (RIGHTROTATE(x, 13)) ^ (RIGHTROTATE(x, 22))
+#define E1(x) (RIGHTROTATE(x, 6) ^ RIGHTROTATE(x, 11) ^ RIGHTROTATE(x, 25))
+#define S0(x) (RIGHTROTATE(x, 7) ^ RIGHTROTATE(x, 18) ^ ((x) >> 3))
+#define S1(x) (RIGHTROTATE(x, 17) ^ RIGHTROTATE(x, 19) ^ ((x) >> 10))
+
+static const uint32_t k[64] = {
+
+}
+
